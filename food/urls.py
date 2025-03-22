46a9e6876dbc769b114c0f159f7df6673dd37f2b
mesteshale <mesteshale@gmail.com>
2025-03-22 10:53:17 +0300

    user registration and login

--- food/urls.py
+++ food/urls.py
@@ -7,5 +7,11 @@ urlpatterns = [
     #/food/item
     path('item/',views.item,name='item'),
     #/food/id
-    path('<int:item_id>/',views.detail,name='detail')
+    path('<int:item_id>/',views.detail,name='detail'),
+    #create item 
+    path('create',views.create_item,name='create_item'),
+    #update item
+    path('update/<int:id>/',views.update_item,name='update_item'),
+    #delete item
+    path('delete/<int:id>/',views.delete_item,name='delete_item')
   ] 
